3549d02d8ec612ff53f91237681f6c020a9491b8
locemar <marcelon27@gmail.com>
2024-04-28 10:35:38 -0300

    feat: implementing database and refactoring methods

--- tests/test_app.py
+++ tests/test_app.py
@@ -3,7 +3,7 @@ from http import HTTPStatus
 import pytest
 from fastapi.testclient import TestClient
 
-from fast_zero.app import app, delete_user, update_user
+from fast_zero.app import app
 
 
 @pytest.fixture()
@@ -11,7 +11,7 @@ def client():
     return TestClient(app)
 
 
-def test_root_deve_retornar_ok_e_ola_mundo(client):
+def test_root_deve_retornar_ok_hello_world(client):
     response = client.get('/')
 
     assert response.status_code == HTTPStatus.OK
@@ -37,8 +37,7 @@ def test_create_user(client):
 
 
 def test_read_users(client):
-    response = client.get('/users/')
-
+    response = client.get('/users')
     assert response.status_code == HTTPStatus.OK
     assert response.json() == {
         'users': [
@@ -51,7 +50,7 @@ def test_read_users(client):
     }
 
 
-def test_update_user(client):
+def test_update_user(client, user):
     response = client.put(
         '/users/1',
         json={
@@ -68,22 +67,26 @@ def test_update_user(client):
     }
 
 
-def test_update_user_exception(client):
-    with pytest.raises(Exception) as e:
-        update_user(12, {})
-    assert e.value.status_code == HTTPStatus.NOT_FOUND
-    assert e.value.detail == 'User not found'
+def test_update_user_exception(client, user):
+    response = client.put(
+        '/users/12',
+        json={
+            'username': 'bob',
+            'email': 'bob@example.com',
+            'password': 'mynewpassword',
+        },
+    )
+    assert response.status_code == HTTPStatus.NOT_FOUND
+    assert response.json() == {'detail': 'User not found'}
 
 
-def test_delete_user(client):
+def test_delete_user(client, user):
     response = client.delete('/users/1')
-
     assert response.status_code == HTTPStatus.OK
     assert response.json() == {'message': 'User deleted'}
 
 
 def test_delete_user_exception(client):
-    with pytest.raises(Exception) as e:
-        delete_user(12)
-    assert e.value.status_code == HTTPStatus.NOT_FOUND
-    assert e.value.detail == 'User not found'
+    response = client.delete('/users/12')
+    assert response.status_code == HTTPStatus.NOT_FOUND
+    assert response.json() == {'detail': 'User not found'}
--- tests/test_db.py
+++ tests/test_db.py
@@ -4,12 +4,10 @@ from fast_zero.models import User
 
 
 def test_create_user(session):
-    new_user = User(
-        username='marcelo', password='secret', email='marcelo@email.com'
-    )
+    new_user = User(username='alice', password='secret', email='teste@test')
     session.add(new_user)
     session.commit()
 
-    user = session.scalar(select(User).where(User.username == 'marcelo'))
+    user = session.scalar(select(User).where(User.username == 'alice'))
 
-    assert user.username == 'marcelo'
+    assert user.username == 'alice'
